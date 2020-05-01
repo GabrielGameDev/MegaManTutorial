ea244e3160addd9bdffc1aadf0a457521623c472
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2020-05-01 11:34:06 -0300

    Player Movement
    Iniciado movimento do jogador

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 5;
+
+    public float jumpForce = 3;
+    public float jumpHoldForce = 1;
+    public float jumpHoldDuration = 0.15f;
+    public bool isJumping;
+    private float jumpTime;
+
     public float horizontal;
     public bool jumpHeld;
+    private bool jumpStarted;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +31,49 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
-    
+
+    private void FixedUpdate()
+    {
+        GroundMovement();
+        AirMovement();
+    }
+
+    void GroundMovement()
+    {
+
+        float xVelocity = speed * horizontal;
+
+        rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+    }
+
+    void AirMovement()
+    {
+        if (jumpStarted)
+        {
+            isJumping = true;
+            jumpStarted = false;
+
+            rb.velocity = Vector2.zero;
+
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            jumpTime = Time.time + jumpHoldDuration;
+        }
+        if (isJumping)
+        {
+            if (jumpHeld)
+            {
+                rb.AddForce(Vector2.up * jumpHoldForce, ForceMode2D.Impulse);
+            }
+
+            if(jumpTime <= Time.time)
+            {
+                isJumping = false;
+            }
+        }
+
+    }
+
     public void Move(InputAction.CallbackContext ctx)
     {
         horizontal = ctx.ReadValue<float>();
@@ -29,7 +83,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (ctx.started)
         {
-            Debug.Log("Iniciou pulo");
+            jumpStarted = true;
         }
 
         jumpHeld = ctx.performed;
