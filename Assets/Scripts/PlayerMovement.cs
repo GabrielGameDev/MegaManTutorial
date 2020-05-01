369295eb08d00d921ceef659b32293efb010f8a4
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2020-05-01 11:05:29 -0300

    Player Input
    Configurando os inputs do jogador

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontal;
+    public bool jumpHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        horizontal = ctx.ReadValue<float>();
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            Debug.Log("Iniciou pulo");
+        }
+
+        jumpHeld = ctx.performed;
+    }
+}
