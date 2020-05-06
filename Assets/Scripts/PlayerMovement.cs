20f3967e697fe433b3482efa6a104c6bf5702de4
Gabriel-PC\Gabriel <gabrielbarbosa@msn.com>
2020-05-06 13:46:40 -0300

    Checagem de física
    Checagem se jogador está no chão. Flip e tempo de coyote

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,17 +13,28 @@ public class PlayerMovement : MonoBehaviour
     public float jumpHoldDuration = 0.15f;
     public bool isJumping;
     private float jumpTime;
+    public bool isOnGround;
 
+    public float coyoteDuration = 0.1f;
+    private float coyoteTime;
+
+    public float leftFootOffset = -0.3f;
+    public float rightFootOffset = 0.3f;
+    public float groundOffset = 0.5f;
+    public float groundDistance = 0.2f;
+    public LayerMask groundLayer;
+
+    private int direction = 1;
     public float horizontal;
     public bool jumpHeld;
-    private bool jumpStarted;
+    public bool jumpStarted;
 
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();        
     }
 
     // Update is called once per frame
@@ -34,22 +45,49 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        PhysicsCheck();
         GroundMovement();
         AirMovement();
     }
 
+    void PhysicsCheck()
+    { 
+        isOnGround = false;
+
+        RaycastHit2D leftFoot = Raycast(new Vector2(leftFootOffset, -groundOffset), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D rightFoot = Raycast(new Vector2(rightFootOffset, -groundOffset), Vector2.down, groundDistance, groundLayer);
+
+        if(leftFoot || rightFoot)
+        {
+            if(!isJumping)
+                isOnGround = true;
+        }
+    }
     void GroundMovement()
     {
 
         float xVelocity = speed * horizontal;
 
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        if(direction * xVelocity < 0)
+        {
+            Flip();
+        }
+
+        if (isOnGround)
+        {
+            coyoteTime = Time.time + coyoteDuration;
+        }
     }
 
     void AirMovement()
     {
-        if (jumpStarted)
+        if (jumpStarted &&(isOnGround || coyoteTime > Time.time))
         {
+
+            isOnGround = false;
+
             isJumping = true;
             jumpStarted = false;
 
@@ -58,6 +96,8 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
             jumpTime = Time.time + jumpHoldDuration;
+
+            coyoteTime = Time.time;
         }
         if (isJumping)
         {
@@ -72,6 +112,17 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        jumpStarted = false;
+
+    }
+
+    void Flip()
+    {
+        direction *= -1;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     public void Move(InputAction.CallbackContext ctx)
@@ -87,5 +138,18 @@ public class PlayerMovement : MonoBehaviour
         }
 
         jumpHeld = ctx.performed;
+    }
+
+    RaycastHit2D Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask layerMask)
+    {
+        Vector2 pos = transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDirection, length, layerMask);
+
+        Color color = hit ? Color.red : Color.green;
+
+        Debug.DrawRay(pos + offset, rayDirection * length, color);
+
+        return hit;
     }
 }
